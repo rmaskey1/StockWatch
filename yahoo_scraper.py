1067a2e06d3f9d029fa0ed982ad5fba72624f3e3
rmaskey1 <maskey.reshaj@gmail.com>
2023-06-16 21:08:27 -0700

    Update yahoo_scraper.py

--- yahoo_scraper.py
+++ yahoo_scraper.py
@@ -1,11 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get('https://finance.yahoo.com/topic/earnings').text
-soup = BeautifulSoup(html_text, 'html.parser')
+def get_links():
+    html_text = requests.get('https://finance.yahoo.com/topic/earnings').text
+    soup = BeautifulSoup(html_text, 'html.parser')
 
-layer = soup.find('div', id = "Fin-Stream").find('ul').find_all('h3')
-print(layer)
-for news in layer:
-    links = news.find_all('a', href=True)
-print(links)
+    layer = soup.find('div', id = "Fin-Stream").find('ul').find_all('h3')
+    #print(layer)
+    links = []
+    for news in layer:
+        links.append(f"https://finance.yahoo.com/{news.find('a', href=True)['href']}")
+    return links
+
+def get_text(index):
+    link = get_links()[index]
+    
+    html_text = requests.get(link).text
+    soup = BeautifulSoup(html_text, 'html.parser')
+
+    article = soup.find('article', role = "article").text
+    print(article)
+
+
+# main
+print(get_links())
