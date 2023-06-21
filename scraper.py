0140aa8a0b7f1d9913a7ae8beb7a0535f4eebf9c
rmaskey1 <maskey.reshaj@gmail.com>
2023-06-21 09:29:50 -0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -26,8 +26,32 @@ def nasdaq_stock_urls():
     #    outfile.write(data)
     return stock_urls
 
-def nasdaq_news_scrape():
-    return
+def nasdaq_news_scrape(stock_code):
+    offset = 0
+    news_urls = []
+    while True:
+        url = f"https://api.nasdaq.com/api/news/topic/articlebysymbol?q={stock_code.lower()}|stocks&offset={offset}&limit=1&fallback=false"
+        data = requests.get(url, headers=header, timeout=5).json()
+        if "hours" in data["data"]["rows"][0]["ago"] and int(data["data"]["rows"][0]["ago"].split()[0]) > 10:
+            break
+        news_urls.append(f"https://www.nasdaq.com/{data['data']['rows'][0]['url']}")
+        offset = offset + 1
+    return news_urls
+
+def yahoo_news_scrape(stock_code):
+    url = f"https://finance.yahoo.com/quote/{stock_code}?p={stock_code}&.tsrc=fin-srch"
+    html_text = requests.get(url, headers=header).text
+    soup = BeautifulSoup(html_text, 'lxml')
+
+    layer = soup.find('ul', class_ = "My(0) P(0) Wow(bw) Ov(h)").find_all('li', class_ = "js-stream-content Pos(r)")
+    print(*layer, sep="\n")
+    news_urls = []
+    for news in layer:
+        print(news.find("div", class_ = "C(#959595) Fz(11px) D(ib) Mb(6px)"))
+        if "hours" in news.find_all("span")[1].text and int(news.find_all("span")[1].text.split()[0]) > 10:
+            break
+        news_urls.append(f"https://finance.yahoo.com/{news.find('a', href=True)['href']}")
+    return news_urls
 
 def real_time_price(stock_code):
     url = f"https://finance.yahoo.com/quote/{stock_code}?p={stock_code}&.tsrc=fin-srch"
@@ -43,5 +67,7 @@ def real_time_price(stock_code):
         return []
     
 
-print(nasdaq_stock_urls())
-print(real_time_price("PRIF"))
+# print(nasdaq_stock_urls())
+print(real_time_price("AAPL"))
+#print(nasdaq_news_scrape("AAPL"))
+#print(yahoo_news_scrape("AAPL"))
