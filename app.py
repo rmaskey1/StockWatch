4c761d66595d1f6d9ec11f49038d23140a954280
rmaskey1 <maskey.reshaj@gmail.com>
2023-06-29 11:14:18 -0700

    venv added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import psycopg2
 from datetime import timedelta
+import finnhub
 from flask import Flask, request, send_file, render_template, redirect, url_for, session
 
 app = Flask(__name__)
@@ -7,10 +8,12 @@ app.secret_key = "stockwatch"
 
 conn = psycopg2.connect(
     host="localhost",
-    database="Test",
+    database="StockWatch",
     user="postgres",
     password="admin"
 )
+
+finnhub_client = finnhub.Client(api_key="cie9espr01qmfas4b430cie9espr01qmfas4b43g")
 
 @app.before_request
 def make_session_permanent():
@@ -20,7 +23,31 @@ def make_session_permanent():
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if "username" in session:
-        return render_template("home.html")
+        if request.method == 'POST':
+            ticker = request.form['ticker']
+            username = session["username"]
+            response = finnhub_client.symbol_lookup(ticker)
+            if response["count"]>0:
+                company_name = response["result"][0]["description"]
+            else:
+                print("Company not found.")
+            response = finnhub_client.quote(ticker)
+            if 'c'in response:
+                current_price = response['c']
+            else:
+                print("Current stock price not available.")
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO stock (ticker, username, company_name, price) VALUES (%s, %s, %s, %s)', (ticker, username, company_name, current_price))
+            conn.commit()
+            cursor.close()
+
+        username = session["username"]
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM stock WHERE username = %s', (username,))
+        stocks = cursor.fetchall()
+        cursor.close()
+
+        return render_template("home.html", stocks=stocks)
     else:
         return redirect(url_for("login"))
 
@@ -74,6 +101,5 @@ def signup():
         return render_template("signup.html", error=False)
 
 if __name__ == '__main__':
-    session.clear()
     app.debug = True
     app.run()
