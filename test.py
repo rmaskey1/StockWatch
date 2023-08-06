3f5543de3dd77c2cba61d0e49d45dfec713de779
rmaskey1 <maskey.reshaj@gmail.com>
2023-08-06 13:58:40 -0700

    frontend changes

--- test.py
+++
@@ -1,9 +0,0 @@
-import yfinance as yf
-
-ticker = yf.Ticker('GOOGL').info
-print(ticker)
-market_price = ticker['currentPrice']
-previous_close_price = ticker['regularMarketPreviousClose']
-print('Ticker: GOOGL')
-print('Market Price:', market_price)
-print('Previous Close Price:', previous_close_price)
